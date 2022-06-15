f3efd464fe7eaf760983dd9e06a5b0c9d49ccfc5
TaariqKhan <taariqkhs@gmail.com>
2022-06-15 16:58:12 +0200

    initial commint

--- src/main/java/za/ac/cput/Helper/helper.java
+++ src/main/java/za/ac/cput/Helper/helper.java
@@ -2,18 +2,24 @@ package za.ac.cput.Helper;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class helper {
-
+public class helper
+{
     public static boolean isEmptyOrNull(String str)
     {
         return StringUtils.isEmpty(str);
     }
+
     public static String setEmptyIfNull(String str)
     {
         if(isEmptyOrNull(str))
-            return "";
-
+            return StringUtils.EMPTY;
 
         return str;
+    }
+
+    public static void checkStringParam(String paramName, String paramValue)
+    {
+        if(isEmptyOrNull(paramValue))
+            throw new IllegalArgumentException(String.format("Invalid value for parameter: %s",paramName));
     }
 }
